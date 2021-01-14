04988ea71870162a72721fa0898f8127b137b0fb
nokranger <anusorn.thavornporn@gmail.com>
2021-01-14 17:49:26 +0700

    add: search object

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -14,78 +14,159 @@
           <b-col>
             <div style="border-radius: 5px;border: thin solid #888;width: 200px;height: 100px;margin:5px;background-color: #00ccff;color: whitesmoke;">
               <div>
-                Author: {{post.title.name}}
+                Author: {{post.name.name}}
               </div>
               <div>
-                QR: {{post.title.qr}}
+                <!-- QR: {{post.title.qr}} -->
               </div>
             </div>
           </b-col>
           <b-col>
           <div style="border-radius: 5px;border: thin solid #888;width: 200px;height: 100px;margin:5px;background-color: #00ccff;color: whitesmoke;">
             <div>
-              Location: {{post.author.name}}
+              <!-- Location: {{post.author.name}} -->
             </div>
             <div>
-              QR: {{post.author.qr}}
+              <!-- QR: {{post.author.qr}} -->
             </div>
           </div>
           </b-col>
         </b-row>
-        <!-- <div>{{filteredList}}</div> -->
       </div>
     </b-container>
   </div>
 </template>
 <script>
+import axios from 'axios'
+// class Post {
+//   constructor (title, link, author, img) {
+//     this.title = title
+//     this.link = link
+//     this.author = author
+//     this.img = img
+//   }
 class Post {
-  constructor (title, link, author, img) {
-    this.title = title
-    this.link = link
-    this.author = author
-    this.img = img
+  constructor (name, lo, nameQrUrl, loQrUrl) {
+    this.name = name
+    this.lo = lo
+    this.name_qr_url = nameQrUrl
+    this.lo_qr_url = loQrUrl
   }
 }
 export default {
   data () {
     return {
       search: '',
-      postLists: [{
-        item: { item: 'nokky', qr: '' },
-        location: { location: '32131231231', qr: '' }
-      }, {
-        item: { item: 'loki', qr: '' },
-        location: { location: '3123123123', qr: '' }
-      }, {
-        item: { item: 'momo', qr: '' },
-        location: { location: '312312312', qr: '' }
-      }],
-      postList: [
-        new Post(
-          { name: 'anusorn', qr: 'rqwrqwrqwrw' },
-          'thavornpon',
-          { name: 'nokky', qr: 'rqwrqwrqwrw' },
-          'fsaasfasfasfafas'
-        ),
-        new Post(
-          { name: 'anusorn', qr: 'rqwrqwrqwrw' },
-          'thavornpon',
-          { name: 'loki', qr: 'rqwrqwrqwrw' },
-          'fsaasfasfasfafas'
-        ),
-        new Post(
-          { name: 'anusorn', qr: 'rqwrqwrqwrw' },
-          'thavornfsfsfpon',
-          { name: 'momo', qr: 'rqwrqwrqwrw' },
-          'fsaas11111fasfasfafas'
+      postList: []
+      // ,
+      // postList: [
+      //   new Post(
+      //     { name: 'anusorn', qr: 'rqwrqwrqwrw' },
+      //     'thavornpon',
+      //     { name: 'nokky', qr: 'rqwrqwrqwrw' },
+      //     'fsaasfasfasfafas'
+      //   ),
+      //   new Post(
+      //     { name: 'anusorn', qr: 'rqwrqwrqwrw' },
+      //     'thavornpon',
+      //     { name: 'loki', qr: 'rqwrqwrqwrw' },
+      //     'fsaasfasfasfafas'
+      //   ),
+      //   new Post(
+      //     { name: 'anusorn', qr: 'rqwrqwrqwrw' },
+      //     'thavornfsfsfpon',
+      //     { name: 'momo', qr: 'rqwrqwrqwrw' },
+      //     'fsaas11111fasfasfafas'
+      //   )
+      // ]
+    }
+  },
+  async created () {
+    await this.getQR()
+  },
+  mounted () {
+    // axios.get('http://192.168.1.34:9080/fetch_item_list').then(response => {
+    //   console.log('APIQRCODE', response)
+    //   console.log(typeof (response))
+    //   this.postLists = new Post(
+    //     {
+    //       name: response.data.item_list.map((data, i) => {
+    //         return {
+    //           name: data.name
+    //         }
+    //       }),
+    //       name_qr_url: response.data.item_list.map((data, i) => {
+    //         return {
+    //           name_qr_url: data.name_qr_url
+    //         }
+    //       })
+    //     },
+    //     {
+    //       lo: response.data.item_list.map((data, i) => {
+    //         return {
+    //           lo: data.lo
+    //         }
+    //       }),
+    //       lo_qr_url: response.data.item_list.map((data, i) => {
+    //         return {
+    //           lo_qr_url: data.lo_qr_url
+    //         }
+    //       })
+    //     }
+    //   )
+    //   // response.data.item_list
+    //   console.log('NEWAPIQRCODE', this.postLists)
+    // })
+  },
+  methods: {
+    getQR () {
+      axios.get('http://192.168.1.34:9080/fetch_item_list').then(response => {
+        console.log('APIQRCODE', response)
+        console.log(typeof (response))
+        this.postList = new Post(
+          {
+            name: response.data.item_list.map((data, i) => {
+              return {
+                name: data.name
+              }
+            })
+          },
+          {
+            name_qr_url: response.data.item_list.map((data, i) => {
+              return {
+                name_qr_url: data.name_qr_url
+              }
+            })
+          },
+          {
+            lo: response.data.item_list.map((data, i) => {
+              return {
+                lo: data.lo
+              }
+            })
+          },
+          {
+            lo_qr_url: response.data.item_list.map((data, i) => {
+              return {
+                lo_qr_url: data.lo_qr_url
+              }
+            })
+          }
         )
-      ]
+        // response.data.item_list
+        console.log('NEWAPIQRCODE', this.postList)
+      })
     }
   },
   computed: {
+    // filteredList () {
+    //   return this.postList.filter(post => {
+    //     return post.author.name.toLowerCase().includes(this.search.toLowerCase())
+    //   })
+    // }
     filteredList () {
       return this.postList.filter(post => {
-        return post.author.name.toLowerCase().includes(this.search.toLowerCase())
+        return post.name.name.toLowerCase().includes(this.search.toLowerCase())
       })
     }
   }
