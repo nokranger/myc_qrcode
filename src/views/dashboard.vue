8ca325f057aa1fbb831098b40d77f94a2c9c04b0
nokranger <anusorn.thavornporn@gmail.com>
2021-01-18 15:06:36 +0700

    add: real data but so slow search

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,172 +1,163 @@
 <template>
   <div>
     <br>
-    <h1>Search</h1>
+    <div>
+    </div>
     <b-container>
-      <b-row>
-        <b-col>
-          <b-input type="text" placeholder="Search title" v-model="search"></b-input>
-        </b-col>
-      </b-row>
+      <b-container fluid>
+        <b-row>
+          <b-col>
+            <div>
+              <b-form-group
+                label="Filter"
+                label-for="filter-input"
+                label-cols-sm="3"
+                label-align-sm="right"
+                label-size="sm"
+                class="mb-0"
+              >
+                <b-input-group size="sm">
+                  <b-form-input
+                    id="filter-input"
+                    v-model="filter"
+                    type="search"
+                    placeholder="Type to Search"
+                  ></b-form-input>
+                  <b-input-group-append>
+                    <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+                  </b-input-group-append>
+                </b-input-group>
+              </b-form-group>
+            </div>
+          </b-col>
+          <b-col>
+            <div>
+              <b-form-checkbox-group
+                v-model="filterOn"
+                class="mt-1"
+              >
+                <b-form-checkbox value="name">Name</b-form-checkbox>
+                <b-form-checkbox value="lo">Location</b-form-checkbox>
+              </b-form-checkbox-group>
+            </div>
+          </b-col>
+          <b-col></b-col>
+        </b-row>
+        <br>
+        <b-row>
+          <b-col>
+            <div>
+              <b-form-select
+                id="per-page-select"
+                v-model="perPage"
+                :options="pageOptions"
+                size="sm"
+              ></b-form-select>
+            </div>
+          </b-col>
+          <b-col>
+            <b-pagination
+              v-model="currentPage"
+              :total-rows="totalRows"
+              :per-page="perPage"
+              align="fill"
+              size="sm"
+              class="my-0"
+            ></b-pagination>
+          </b-col>
+        </b-row>
+      </b-container>
       <br>
-      <div style="border-radius: 5px;border: thin solid #888;margin:5px;width:500px;float: left;" v-for="(post, index) in filteredList" :key="index">
+      <b-table :items="postList" :fields="fields" :current-page="currentPage" :per-page="perPage" :filter="filter" :filter-included-fields="filterOn" @filtered="onFiltered">
+        <template v-slot:cell(name)="data">
+          <div style="border-radius: 5px;border: thin solid #888;width: 200px;height: 100px;margin:5px;background-color: #00ccff;color: whitesmoke;">
+            <div>
+              {{data.item.name}}
+            </div>
+            <div>
+              <img style="width:70px;height:70px;" :src="data.item.name_qr_url">
+            </div>
+          </div>
+        </template>
+        <template v-slot:cell(lo)="data">
+          <div style="border-radius: 5px;border: thin solid #888;width: 200px;height: 100px;margin:5px;background-color: #00ccff;color: whitesmoke;">
+            <div>
+              {{data.item.lo}}
+            </div>
+            <div>
+              <img style="width:70px;height:70px;" :src="data.item.lo_qr_url">
+            </div>
+          </div>
+        </template>
+      </b-table>
+      <!-- <div style="border-radius: 5px;border: thin solid #888;margin:5px;width:500px;float: left;" v-for="(post, index) in filteredList" :key="index" :current-page="currentPage" >
         <b-row>
           <b-col>
             <div style="border-radius: 5px;border: thin solid #888;width: 200px;height: 100px;margin:5px;background-color: #00ccff;color: whitesmoke;">
               <div>
-                Author: {{post.name.name}}
+                Name: {{post.name}}
               </div>
               <div>
-                <!-- QR: {{post.title.qr}} -->
+                <img style="width50px;height:50px;" :src="post.name_qr_url" alt="">
               </div>
             </div>
           </b-col>
           <b-col>
           <div style="border-radius: 5px;border: thin solid #888;width: 200px;height: 100px;margin:5px;background-color: #00ccff;color: whitesmoke;">
             <div>
-              <!-- Location: {{post.author.name}} -->
+              Location: {{post.lo}}
             </div>
             <div>
-              <!-- QR: {{post.author.qr}} -->
+              <img style="width50px;height:50px;" :src="post.lo_qr_url" alt="">
             </div>
           </div>
           </b-col>
         </b-row>
-      </div>
+      </div> -->
     </b-container>
   </div>
 </template>
 <script>
 import axios from 'axios'
-// class Post {
-//   constructor (title, link, author, img) {
-//     this.title = title
-//     this.link = link
-//     this.author = author
-//     this.img = img
-//   }
-class Post {
-  constructor (name, lo, nameQrUrl, loQrUrl) {
-    this.name = name
-    this.lo = lo
-    this.name_qr_url = nameQrUrl
-    this.lo_qr_url = loQrUrl
-  }
-}
 export default {
   data () {
     return {
       search: '',
-      postList: []
-      // ,
-      // postList: [
-      //   new Post(
-      //     { name: 'anusorn', qr: 'rqwrqwrqwrw' },
-      //     'thavornpon',
-      //     { name: 'nokky', qr: 'rqwrqwrqwrw' },
-      //     'fsaasfasfasfafas'
-      //   ),
-      //   new Post(
-      //     { name: 'anusorn', qr: 'rqwrqwrqwrw' },
-      //     'thavornpon',
-      //     { name: 'loki', qr: 'rqwrqwrqwrw' },
-      //     'fsaasfasfasfafas'
-      //   ),
-      //   new Post(
-      //     { name: 'anusorn', qr: 'rqwrqwrqwrw' },
-      //     'thavornfsfsfpon',
-      //     { name: 'momo', qr: 'rqwrqwrqwrw' },
-      //     'fsaas11111fasfasfafas'
-      //   )
-      // ]
+      postList: [],
+      fields: [{ key: 'name', label: '', thStyle: { display: 'none' } }, { key: 'lo', label: '', thStyle: { display: 'none' } }],
+      totalRows: 1,
+      currentPage: 1,
+      perPage: 20,
+      pageOptions: [10, 20, 30, 50, 100, { value: 200, text: 'Show a lot' }],
+      pages: [],
+      filter: null,
+      filterOn: []
     }
   },
-  async created () {
-    await this.getQR()
+  beforeCreate () {
+  },
+  created () {
   },
   mounted () {
-    // axios.get('http://192.168.1.34:9080/fetch_item_list').then(response => {
-    //   console.log('APIQRCODE', response)
-    //   console.log(typeof (response))
-    //   this.postLists = new Post(
-    //     {
-    //       name: response.data.item_list.map((data, i) => {
-    //         return {
-    //           name: data.name
-    //         }
-    //       }),
-    //       name_qr_url: response.data.item_list.map((data, i) => {
-    //         return {
-    //           name_qr_url: data.name_qr_url
-    //         }
-    //       })
-    //     },
-    //     {
-    //       lo: response.data.item_list.map((data, i) => {
-    //         return {
-    //           lo: data.lo
-    //         }
-    //       }),
-    //       lo_qr_url: response.data.item_list.map((data, i) => {
-    //         return {
-    //           lo_qr_url: data.lo_qr_url
-    //         }
-    //       })
-    //     }
-    //   )
-    //   // response.data.item_list
-    //   console.log('NEWAPIQRCODE', this.postLists)
-    // })
+    this.getQR()
   },
   methods: {
     getQR () {
-      axios.get('http://192.168.1.34:9080/fetch_item_list').then(response => {
-        console.log('APIQRCODE', response)
-        console.log(typeof (response))
-        this.postList = new Post(
-          {
-            name: response.data.item_list.map((data, i) => {
-              return {
-                name: data.name
-              }
-            })
-          },
-          {
-            name_qr_url: response.data.item_list.map((data, i) => {
-              return {
-                name_qr_url: data.name_qr_url
-              }
-            })
-          },
-          {
-            lo: response.data.item_list.map((data, i) => {
-              return {
-                lo: data.lo
-              }
-            })
-          },
-          {
-            lo_qr_url: response.data.item_list.map((data, i) => {
-              return {
-                lo_qr_url: data.lo_qr_url
-              }
-            })
-          }
-        )
-        // response.data.item_list
-        console.log('NEWAPIQRCODE', this.postList)
+      axios.get('http://192.168.1.41:9080/fetch_item_list').then(response => {
+        this.postList = response.data.item_list
+        this.totalRows = this.postList.length
       })
+    },
+    onFiltered (filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length
+      this.currentPage = 1
     }
   },
   computed: {
-    // filteredList () {
-    //   return this.postList.filter(post => {
-    //     return post.author.name.toLowerCase().includes(this.search.toLowerCase())
-    //   })
-    // }
     filteredList () {
       return this.postList.filter(post => {
-        return post.name.name.toLowerCase().includes(this.search.toLowerCase())
+        return post.name.toLowerCase().includes(this.search.toLowerCase())
       })
     }
   }
